419214a42edfc38c53ab2dfb3e079e0135d4b517
Shaurya1409 <shauryasrivastava14@gmail.com>
2020-07-04 03:06:27 +0530

    CSS Reduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'NUWeb';
+  ngOnInit() {
+    this.loadScript('https://platform.twitter.com/widgets.js');
+  }
+
+  public loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
 }
