7bdf03f30f2a3f968a7d68887aadc9df82bc290f
Anirudh Verma <an649hd@gmail.com>
2020-07-04 08:57:38 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component'
+import { AdmissionsComponent } from './admissions/admissions.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomePageComponent},
+  {path: 'admissions', component: AdmissionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
